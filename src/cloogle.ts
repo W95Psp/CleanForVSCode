4ba543c694402436764470b05b11a21acf81dee7
Erin van der Veen <erin@erinvanderveen.nl>
2021-01-14 16:38:06 +0100

    Consistent codestyle
    - Use tabs at beginning of line
    - Remove trailing whitespace
    - Use switch instead of inline-if chain

--- src/cloogle.ts
+++ src/cloogle.ts
@@ -4,77 +4,100 @@ import {Let} from './tools';
 import * as Request from 'request';
 
 const getContent = function(url: string, ua: string) {
-    return new Promise<string>((resolve, reject) => 
-        Request(url, {
-            headers: {'User-Agent': ua}
-        }, (error, res, body) => error ? reject(error) : resolve(body.toString()))
-    )};
+	return new Promise<string>((resolve, reject) =>
+		Request(url, {
+			headers: {'User-Agent': ua}
+		}, (error, res, body) => error ? reject(error) : resolve(body.toString()))
+	)};
 
-type cloogleResults={return: number, data: cloogleResult[], msg: string, more_available: number};
-type cloogleResult =    ['FunctionResult', [cloogleResult_GeneralData, cloogleResult_Function]]    |
-                        ['TypeResult', [cloogleResult_GeneralData, cloogleResult_Type]]            |
-                        ['ClassResult', [cloogleResult_GeneralData, cloogleResult_Class]]          |
-                        ['SyntaxResult', [cloogleResult_GeneralData, cloogleResult_Syntax]]          |
-                        ['ModuleResult', [cloogleResult_GeneralData, cloogleResult_Module]];
+type cloogleResults =
+	{ return: number
+	, data: cloogleResult[]
+	, msg: string
+	, more_available: number
+	};
+
+type cloogleResult
+	= ['FunctionResult', [cloogleResult_GeneralData, cloogleResult_Function]]
+	| ['TypeResult', [cloogleResult_GeneralData, cloogleResult_Type]]
+	| ['ClassResult', [cloogleResult_GeneralData, cloogleResult_Class]]
+	| ['SyntaxResult', [cloogleResult_GeneralData, cloogleResult_Syntax]]
+	| ['ModuleResult', [cloogleResult_GeneralData, cloogleResult_Module]]
+	;
 
 interface cloogleResult_GeneralData {
-    library: string;
-    filename: string;
-    modul: string;
-    dcl_line: number;
-    icl_line: number;
-    distance: number;
-    builtin?: boolean;
+	library: string;
+	filename: string;
+	modul: string;
+	dcl_line: number;
+	icl_line: number;
+	distance: number;
+	builtin?: boolean;
 }
+
 interface cloogleResult_Function {
-    func: string;
-    kind: string[];
+	func: string;
+	kind: string[];
 }
+
 interface cloogleResult_Type {
-    type: string;
-    // and some other stuff we don't care about here    
+	type: string;
+	// and some other stuff we don't care about here
 }
+
 interface cloogleResult_Class {
-    class_name: string;
-    class_heading: string[];
-    class_funs: string[];
-    // and some other stuff we don't care about here
+	class_name: string;
+	class_heading: string[];
+	class_funs: string[];
+	// and some other stuff we don't care about here
 }
+
 interface cloogleResult_Module {
-    module_is_core: boolean;
+	module_is_core: boolean;
 }
+
 interface cloogleResult_Syntax {
-    syntax_title: boolean;
-    syntax_code: string[];
-    syntax_examples: {example: string, cleanjs_examples: string}[];
+	syntax_title: boolean;
+	syntax_code: string[];
+	syntax_examples: {example: string, cleanjs_examples: string}[];
 }
 
 export async function askCloogle (name) {
-    let result: cloogleResults;
-    try{
-        let content = await getContent('http://cloogle.org/api.php?str='+encodeURI('exact '+name), 'clean-vscode');
-        result = JSON.parse(content) as cloogleResults;
-    }catch(e){
-        console.log('Got error', e);
-        result = {return: -1} as cloogleResults;
-    }
-    return result.data || [];
+	let result: cloogleResults;
+	try {
+		let content = await getContent('http://cloogle.org/api.php?str='+encodeURI('exact '+name), 'clean-vscode');
+		result = JSON.parse(content) as cloogleResults;
+	} catch(e) {
+		console.log('Got error', e);
+		result = {return: -1} as cloogleResults;
+	}
+	return result.data || [];
 }
+
 let cache = new Map<string,cloogleResult>();
+
 export let askCloogleExact = async (name) => {
-    if(cache.has(name))
-        return cache.get(name);
-    let result = (await askCloogle(name))[0];
-    cache.set(name, result);
-    return result;
+	if (cache.has(name))
+		return cache.get(name);
+	let result = (await askCloogle(name))[0];
+	cache.set(name, result);
+	return result;
 }
-export let getInterestingStringFrom = ([typeData, [general, specs]]: cloogleResult): string|string[] => 
-    typeData == 'FunctionResult' ?  (<cloogleResult_Function>   specs).func :
-    typeData == 'TypeResult' ?      (<cloogleResult_Type>       specs).type :
-    typeData == 'SyntaxResult' ? Let(<cloogleResult_Syntax>     specs, s => 
-                                            [/*s.syntax_title,*/s.syntax_code.join(', '), ...Let(
-                                                s.syntax_examples.map(o => o.example),
-                                                l => l.length ? ['> Examples: ', ...l] : []
-                                            )]):
-    typeData == 'ClassResult' ?     (<cloogleResult_Class>      specs).class_funs.join('\n') :
-                                    ''/*(<cloogleResult_Module>     specs)*/;
+
+export let getInterestingStringFrom = ([typeData, [general, specs]]: cloogleResult): string | string[] => {
+	switch(typeData) {
+		case "FunctionResult":
+			return (<cloogleResult_Function> specs).func;
+		case "TypeResult":
+			return (<cloogleResult_Type> specs).type;
+		case "SyntaxResult":
+			let s = <cloogleResult_Syntax> specs;
+			let examples = s.syntax_examples.map(o => o.example);
+			return
+				[ s.syntax_code.join(', ') ].concat(examples.length ? ['> Examples: ', ...examples] : []);
+		case "ClassResult":
+			return (<cloogleResult_Class> specs).class_funs.join('\n');
+		default:
+			return "";
+	}
+}
--- src/cpm.ts
+++ src/cpm.ts
@@ -4,56 +4,60 @@ import {parse, format, normalize} from 'path';
 import {Tuple, Let, ithrow} from './tools';
 import * as VC from 'vscode';
 
-export let useBOW = VC.workspace.getConfiguration("cleanlang").useBashOnWindowsIfPossible
-                    && /^win/.test(process.platform)
-                    && existsSync('C:/Windows/System32/bash.exe');
-let getCleanHomePath = () => 
-    (useBOW ? 
-                 execSync('bash -c "echo $CLEAN_HOME"').toString() : process.env.CLEAN_HOME) 
-            || Let('Error: CLEAN_HOME environment path is not set', e => (VC.window.showErrorMessage(e), ithrow(e)));       
+export let useBOW =
+	VC.workspace.getConfiguration("cleanlang").useBashOnWindowsIfPossible
+	&& /^win/.test(process.platform)
+	&& existsSync('C:/Windows/System32/bash.exe');
+
+let getCleanHomePath = () =>
+	(useBOW ? execSync('bash -c "echo $CLEAN_HOME"').toString() : process.env.CLEAN_HOME)
+	|| Let('Error: CLEAN_HOME environment path is not set', e => (VC.window.showErrorMessage(e), ithrow(e)));
 
 let cleanHome = parse(normalize(getCleanHomePath()));
 let cleanLib = normalize(format(cleanHome) + '/lib');
 
-let readdirPromise = (path: string) => new Promise<string[]>((a,r) => readdir(path, (err,res) => err ? r(err) : a(res)));
-let readFilePromise = (path: string) => new Promise<string>((a,r) => readFile(normalize(path), (err,res) => err ? a('') : a(res.toString())));
+let readdirPromise = (path: string) =>
+	new Promise<string[]>((a,r) => readdir(path, (err,res) => err ? r(err) : a(res)));
+
+let readFilePromise = (path: string) =>
+	new Promise<string>((a,r) => readFile(normalize(path), (err,res) => err ? a('') : a(res.toString())));
 
 let searchRecursivelyForDCLs = async (path: string) : Promise<[string, string[]][]> => {
-    let lst = (await readdirPromise(path)).map(o => path+'/'+o)
-                .filter(p => Let(parse(p).ext, e => !e || e=='.dcl'));
-    let [dirs, files] = lst.split<string, string, string[][]>(<any>(o => lstatSync(o).isDirectory()), (l, r) => [l, r]);
-    let dcls = files.filter(o => parse(o).ext == '.dcl');
-    let modules = (await dcls.mapPromises(async o => 
-        ((await readFilePromise(o)).match(/definition module .*/g)||[]).map(o => o.substr(18).trim().split('.'))
-    )).flatten().getUniqueValues().map(m => Tuple(path, m));
+	let lst = (await readdirPromise(path)).map(o => path+'/'+o).filter(p => Let(parse(p).ext, e => !e || e=='.dcl'));
+	let [dirs, files] = lst.split<string, string, string[][]>(<any>(o => lstatSync(o).isDirectory()), (l, r) => [l, r]);
+	let dcls = files.filter(o => parse(o).ext == '.dcl');
+	let modules = (await dcls.mapPromises(async o =>
+		((await readFilePromise(o)).match(/definition module .*/g)||[]).map(o => o.substr(18).trim().split('.'))
+	)).flatten().getUniqueValues().map(m => Tuple(path, m));
 
-    return modules.concat((await dirs.mapPromises(searchRecursivelyForDCLs)).flatten());
+	return modules.concat((await dirs.mapPromises(searchRecursivelyForDCLs)).flatten());
 }
 
-export let getProjectPath = (dir: string) : string | Error => 
-    readdirSync(dir).some(o => o.slice(-4)=='.prj') ? dir :
-        parse(dir).root==dir ? new Error('No project file found') : getProjectPath(normalize(dir+'/..'));
+export let getProjectPath = (dir: string) : string | Error =>
+	readdirSync(dir).some(o => o.slice(-4)=='.prj') ? dir :
+		parse(dir).root==dir ? new Error('No project file found') : getProjectPath(normalize(dir+'/..'));
 
 let command = useBOW ? ['bash.exe', '-c', 'source ~/.profile; cpm make'] : ['cpm', 'make'];
 
 let treatResult = (s: string) => (console.log(s), /^No project file found/.test(s) ? new Error('CPM: '+s) : s);
 
-export let cpm = async (name: string, newLine?: (line: string) => any) => 
-    new Promise<string | Error>((a,r) => {
-        let p = spawn(command[0], command.slice(1));
-        let left = '';
-        let lines = [];
-        let addl = (l: string) => (lines.push(l), newLine && newLine(l), l);
-        let end = () => a(treatResult(lines.join('\n')));
-        p.stdout.on("data", (chunk) => {
-            let s = chunk.toString();
-            let i = s.lastIndexOf('\n');
-            if(i > -1){
-                addl(left + s.substr(0, i))
-                left = s.substr(i + 1);
-            }else
-                left += s;
-        });
-        p.stdout.on("error", (err) => (addl('ERROR: '+err.toString()), end()));
-        p.stdout.on("close", () => ([left, ' ', 'Program done'].map(x => x && addl(x), end())));
-    });
+export let cpm = async (name: string, newLine?: (line: string) => any) =>
+	new Promise<string | Error>((a,r) => {
+		let p = spawn(command[0], command.slice(1));
+		let left = '';
+		let lines = [];
+		let addl = (l: string) => (lines.push(l), newLine && newLine(l), l);
+		let end = () => a(treatResult(lines.join('\n')));
+		p.stdout.on("data", (chunk) => {
+			let s = chunk.toString();
+			let i = s.lastIndexOf('\n');
+			if (i > -1) {
+			    addl(left + s.substr(0, i))
+			    left = s.substr(i + 1);
+			} else {
+			    left += s;
+			}
+		});
+		p.stdout.on("error", (err) => (addl('ERROR: '+err.toString()), end()));
+		p.stdout.on("close", () => ([left, ' ', 'Program done'].map(x => x && addl(x), end())));
+	});
